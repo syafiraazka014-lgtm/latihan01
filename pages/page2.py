dbf224a24afcafa56474babcf3d8b38d916f664a
unknown <syafiraazka014@gmail.com>
2025-11-20 14:11:57 +0700

    Menambahkan file page1.py, page2.py, page3.py

---
+++ pages/page2.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import plotly.express as px
+import numpy as np
+
+st.title("Data Visualization")
+
+# Generate some data
+x = np.linspace(0, 10, 100)
+y= np.sin(x)
+# plot data
+fig,ax = plt.subplots()
+ax.plot(x, y)
+# Display the plot
+st.pyplot(fig)
---
+++ pages/page3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Settings")
+st.write("The page allows you to customize your preferences.")
